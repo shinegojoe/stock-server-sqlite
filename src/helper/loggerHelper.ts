e10d9f3cbe80e244faf75582edf644866bfae19a
taka <taka@huangguanghongdeMac-mini.local>
2021-03-30 10:26:40 +0800

    update log file name

--- src/helper/loggerHelper.ts
+++ src/helper/loggerHelper.ts
@@ -1,10 +1,12 @@
 const winston = require('winston')
-
 const date = new Date()
 
 const x = date.toLocaleTimeString().split(' ')
-// console.log(x)
-const fileName = `${date.toLocaleDateString()}_${x[0]}`
+let y: any = date.toLocaleDateString()
+y = y.replace(new RegExp('-',"gm"), "_")
+console.log(y)
+
+const fileName = `${y}_${x[0]}`
 // console.log(fileName)
 
 const fileLogger = winston.createLogger({
